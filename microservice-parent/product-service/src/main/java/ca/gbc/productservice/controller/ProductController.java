b9fd364bc0b648a279bf616ef5af975986db2ef4
Hardik Kumar . <hardikgugnani123@gmail.com>
2024-10-29 15:01:54 -0400

    added tests and fixed controller, ProductServiceImpl.java

--- microservice-parent/product-service/src/main/java/ca/gbc/productservice/controller/ProductController.java
+++ microservice-parent/product-service/src/main/java/ca/gbc/productservice/controller/ProductController.java
@@ -1,4 +1,5 @@
 package ca.gbc.productservice.controller;
+
 import ca.gbc.productservice.dto.ProductRequest;
 import ca.gbc.productservice.dto.ProductResponse;
 import ca.gbc.productservice.service.ProductService;
@@ -13,31 +14,40 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
-
 public class ProductController {
+
     private final ProductService productService;
+
+    // Create a new product
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-
-    public void createProduct(@RequestBody ProductRequest productRequest){
+    public void createProduct(@RequestBody ProductRequest productRequest) {
         productService.createProduct(productRequest);
     }
 
+    // Get all products
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<ProductResponse> getAllProducts(){
+    public List<ProductResponse> getAllProducts() {
         return productService.getAllProducts();
     }
 
-    @PutMapping
+    // Update a product
+    @PutMapping("/{productId}")
     public ResponseEntity<?> updateProduct(@PathVariable("productId") String productId,
-                                           @RequestBody ProductRequest productRequest){
-        String updateProductId = productService.updateProduct(productId,productRequest);
+                                           @RequestBody ProductRequest productRequest) {
+        String updateProductId = productService.updateProduct(productId, productRequest);
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Location", "/api/product/");
+        headers.add("Location", "/api/product/" + updateProductId);
 
-        return new ResponseEntity<>(headers,HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(headers, HttpStatus.NO_CONTENT);
     }
 
+    // Delete a product
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable("productId") String productId) {
+        productService.deleteProduct(productId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- microservice-parent/product-service/src/test/java/ca/gbc/productservice/ProductServiceApplicationTests.java
+++ microservice-parent/product-service/src/test/java/ca/gbc/productservice/ProductServiceApplicationTests.java
@@ -1,15 +1,118 @@
 package ca.gbc.productservice;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+// This annotation is used in combination with TestContainers to automatically configure the connection to
+// the Test MongoDbContainer
+
+
+@LocalServerPort
+private Integer port;
+
+@BeforeEach
+void setup(){
+
+	RestAssured.baseURI = "http://localhost";
+	RestAssured.port = port;
+}
+
+
+
+@ServiceConnection
+static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+static {
+		mongoDBContainer.start();
+	}
+
+@Test
+	void createProductTest() {
+
+	String requestBody = """
+			{
+			"name" : "Samsung TV",
+			"description" : "Samsung TV - Model 2024",
+			"price" : 2000
+			
+			}
+			""";
+
+		//BDD - -0 Behavioural Driven Development (Given,When,Then)
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("Samsung TV"))
+				.body("description", Matchers.equalTo("Samsung TV - Model 2024"))
+				.body("price", Matchers.equalTo(2000));
+
+
+}
+    @Test
+	void getAllProductsTest() {
+
+		String requestBody = """
+			{
+			"name" : "Samsung TV",
+			"description" : "Samsung TV - Model 2024",
+			"price" : 2000
+			
+			}
+			""";
+
+		//BDD - -0 Behavioural Driven Development (Given,When,Then)
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("Samsung TV"))
+				.body("description", Matchers.equalTo("Samsung TV - Model 2024"))
+				.body("price", Matchers.equalTo(2000));
+
+ // We are creating this upper code again in getAllProducts() because these tests will exist seperately from each other
+		// IF we don't do this then getAllProducts won't be able to read the product created
+		//(because it doesn't exist), and return an empty response.
+
+		RestAssured.given()
+				.contentType("application/json")
+				.when()
+				.get("/api/product")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.body("size()", Matchers.greaterThan(0))
+				.body("[0].name",Matchers.equalTo("Samsung TV"))
+				.body("[0].description", Matchers.equalTo("Samsung TV - Model 2024"))
+				.body("[0].price", Matchers.equalTo(2000));
+
+
+
+
+
+
+
+
 	}
 
 }
